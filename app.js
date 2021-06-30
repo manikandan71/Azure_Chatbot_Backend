e8bdd727c5c4f16ea68ce35d0d6c932a2ecbf179
Manikandan <mani.zenix@gmail.com>
2021-06-30 11:26:39 +0530

    server code

---
+++ app.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var http = require("http");
+// var FormData = require('form-data');
+// var form = new FormData();
+const https = require("https");
+const fs = require("fs");
+var express = require("express");
+// var axios = require("axios");
+var app = express();
+// var ab = require("jquery");
+var cors = require("cors");
+app.use(cors());
+
+app.get("/chatbot", function (req, res) {
+var options = {
+  'method': 'POST',
+  'url': 'https://directline.botframework.com/v3/directline/conversations',
+  'headers': {
+    'Authorization': ''
+  },
+  formData: {
+    // userID: '4b9cc842-db4b-419d-b8cb-1ef0b1d1aed0',
+    // 'username': 'ngrbot',
+    // 'locale': 'en-US'
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+  console.log(error);
+  res.send(response);
+});
+});
+
+       var key = fs.readFileSync('/etc/letsencrypt/live/api.ittstar.ai/privkey.pem');
+      var cert = fs.readFileSync('/etc/letsencrypt/live/api.ittstar.ai/fullchain.pem');
+      var options = {
+        key: key,
+        cert: cert
+      };
+https.createServer(options,app ).listen(4000);
+      
+console.log('Server running at http://127.0.0.1:4000/');
